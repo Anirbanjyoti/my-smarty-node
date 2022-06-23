8fef5e88fb74190d59a3ef40456bfdd09ac7f6e6
Anirbanjyoti <anirbanjyoti708@gmail.com>
2022-06-23 22:42:29 +0530

    node serverand express server install and making ready steps

---
+++ __steps.js
@@ -0,0 +1,20 @@
+/**
+ * Basic Server Setup
+======================
+ * 1. Create folder manually or mkdir
+* 2.open Command line on that folder
+* 3. npm Init
+ * 4. npm install Express
+ * 5. Create Node.js
+ * For express /Node server
+ * ============
+ * Require express 
+ * create app variable
+ * Declare port
+ * set app.get('/)
+ * listen to port 
+ * node index.js
+ * 
+ * 
+ * 
+ */
